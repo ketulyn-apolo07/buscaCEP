844d27b331788db8007e37f29726a11d3d84b8bf
apolodeus do sol-cyber <ketulynsilveira@gmail.com>
2025-10-23 15:01:24 -0300

    CEP

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ const txt_rua = document.querySelector("#rua");
 const txt_numero =document.querySelector("#numero");
 const txt_cidade = document.querySelector("#cidade");
 const txt_bairro = document.querySelector("#bairro");
+const txt_complemento = document.querySelector("#complemento");
 const slt_estado = document.querySelector("#estado");
+
+const err_cep = document.querySelector("#cep-erro");
 
 const loadingOverlay = document.querySelector("#loadingOverlay");
 
@@ -16,13 +19,14 @@ const loadingOverlay = document.querySelector("#loadingOverlay");
 //-------------------------------------------------------------------
 
        function consultaCEP() {
-      let cep = txt_cep.value;
 
-         if (cep.match(/^\d{5}-\d{3}$/)) {
+        limpaCampos();
+      
+        let cep = txt_cep.value;
 
-       cep = cep.replace("-", "");
+        if (cep.match(/^\d{5}-\d{3}$/)) {
 
-       limpaCampos();
+        cep = cep.replace("-", "");
 
        loadingOverlay.classList.add('d-flex');
        loadingOverlay.classList.remove('d-none');
@@ -35,6 +39,8 @@ const loadingOverlay = document.querySelector("#loadingOverlay");
                 //converte a resposta para JSON.
                 return response.json();
             })
+        
+
             .then(function(jsonResponse) {
                 console.log(jsonResponse);
 
@@ -58,23 +64,32 @@ const loadingOverlay = document.querySelector("#loadingOverlay");
                     if (jsonResponse.uf !== "") {
                         slt_estado.value = jsonResponse.uf;
                         slt_estado.disabled = true;
+                    
                     }
-                }
-            });
+                } 
+            })
+         .catch(error => {
+            loadingOverlay.classList.add('d-none');
+            loadingOverlay.classList.remove('d-flex');
 
-    }
-}
+            err_cep.innerHTML = "falha na consulta do CEP.\
+                                 <a href='#' onclick='consultaCEP()'>Tentar novamente?</a>"
+            txt_cep.classList.add("is-invalid");
+        });
+     }
+ }
 function limpaCampos() {
     txt_rua.value = "";
     txt_cidade.value = "";
     txt_bairro.value = "";
     txt_numero.value = "";
+    txt_complemento.value = "";
     slt_estado.value = "";
 
-    txt_rua.disabled = true;
-    txt_cidade.disabled = true;
-    txt_bairro.disabled = true;
-    slt_estado.disabled = true;
+    txt_rua.disabled = false;
+    txt_cidade.disabled = false;
+    txt_bairro.disabled = false;
+    slt_estado.disabled = false;
 }
 
 //-------------------------------------------------------------------
